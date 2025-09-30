3ccf0891fa9e5119fc4c8d44d7740e7956f93bb8
samadrita.saha <samadrita.saha@rwth-aachen.de>
2025-09-30 14:08:06 +0200

    include zoned time for token based fitness evaluator

--- org.pm4knime.python/nodes/token_fitness.py
+++ org.pm4knime.python/nodes/token_fitness.py
@@ -40,7 +40,7 @@ class FitnessChecker:
                                               description="The column that contains the timestamps."
                                                           "This column must have the type 'Local Date Time'.",
                                               port_index=0,
-                                              column_filter=knime_util.is_date)
+                                              column_filter=knime_util.is_type_timestamp)
 
     def configure(self, configure_context: knext.ConfigurationContext, input_schema_1: knext.Schema,
                   petri_net_spec: PetriNetSpec):
@@ -54,8 +54,7 @@ class FitnessChecker:
         
         net, initial_marking, final_marking = convert_port_object_to_pm4py(petri_net)
 
-        event_log[self.column_param_time + "UTC"] = pd.to_datetime(event_log[self.column_param_time],
-                                                                   format='%Y-%m-%d %H:%M:%S').dt.tz_localize(pytz.utc)
+        event_log[self.column_param_time + "UTC"] = pd.to_datetime(event_log[self.column_param_time], utc=True)
         event_log = event_log.sort_values(by=[self.column_param_case, self.column_param_time + "UTC"])
 
         fitness = pm4py.fitness_token_based_replay(log=event_log,
