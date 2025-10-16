489be39ec8b39cb8328dd9e8fce37d3803240b2e
Samadrita Saha <samadrita.saha@fit.fraunhofer.de>
2025-10-16 17:38:15 +0200

    updated empty settings nodes

--- org.pm4knime.java/src/org/pm4knime/util/defaultnode/EmptyNodeSettings.java
+++ org.pm4knime.java/src/org/pm4knime/util/defaultnode/EmptyNodeSettings.java
@@ -1,33 +1,41 @@
 package org.pm4knime.util.defaultnode;
 
 import org.knime.node.parameters.NodeParameters;
-
+import org.knime.node.parameters.Widget;
+import org.knime.node.parameters.layout.Layout;
+import org.knime.node.parameters.layout.Section;
+import org.knime.node.parameters.updates.Effect;
+import org.knime.node.parameters.updates.EffectPredicate;
+import org.knime.node.parameters.updates.EffectPredicateProvider;
+import org.knime.node.parameters.updates.ParameterReference;
+import org.knime.node.parameters.updates.ValueReference;
+import org.knime.node.parameters.updates.Effect.EffectType;
 
  
 public final class EmptyNodeSettings implements NodeParameters {
 	
-//	interface NodeSignal {}
-//	
-//	public static interface EmptyLayout {
-//				
-//	    @Section(title = "This node has no settings!")
-//        interface Text {
-//	    	
-//        }   
-//	      
-//	 }
-//		
-//	static final class ShiftModeIsDurationColumn implements PredicateProvider {
-//
-//	    @Override
-//	    public Predicate init(final PredicateInitializer i) {
-//	         return i.getEnum(ShiftModeRef.class).isOneOf(ShiftMode.PERIOD_COLUMN);
-//	    }
-//	}
-//	
-//	@Layout(EmptyLayout.Text.class)
-//	@Signal(id = NodeSignal.class, condition = TrueCondition.class)
-//	@Effect(signals = NodeSignal.class, type = EffectType.HIDE)
-//	boolean dummy_var = true;
-		
-}
+	static final class NodeSignal implements ParameterReference<Boolean> {}
+	
+	public static interface EmptyLayout {
+				
+	    @Section(title = "This node has no settings!")
+        interface Text {
+	    	
+        }   
+	      
+	 }
+	
+	static final class AlwaysTrue implements EffectPredicateProvider {
+        @Override
+        public EffectPredicate init(final PredicateInitializer i) {
+            return i.getBoolean(NodeSignal.class).isTrue();
+        }
+    }
+
+	
+	@Layout(EmptyLayout.Text.class)
+	@Effect(predicate = AlwaysTrue.class, type = EffectType.HIDE)
+	@Widget(title = ".", description = ".")
+	@ValueReference(NodeSignal.class)
+	boolean dummy_var = true;
+	}
