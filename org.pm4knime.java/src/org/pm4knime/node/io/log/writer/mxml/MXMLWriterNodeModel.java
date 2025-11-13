f94656eed8f03029887fd24a8018368352081dc1
samadrita.saha <samadrita.saha@rwth-aachen.de>
2025-11-13 07:47:48 +0100

    updated mxml writer with FileSelection

--- org.pm4knime.java/src/org/pm4knime/node/io/log/writer/mxml/MXMLWriterNodeModel.java
+++ org.pm4knime.java/src/org/pm4knime/node/io/log/writer/mxml/MXMLWriterNodeModel.java
@@ -1,33 +1,36 @@
 package org.pm4knime.node.io.log.writer.mxml;
 
 import java.io.IOException;
-import org.deckfour.xes.model.XLog;
-import org.deckfour.xes.out.XSerializer;
 import java.io.OutputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Locale;
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.out.XMxmlGZIPSerializer;
+import org.deckfour.xes.out.XMxmlSerializer;
+import org.deckfour.xes.out.XSerializer;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
 import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.util.CheckUtils;
 import org.knime.core.util.FileUtil;
 import org.knime.core.webui.node.impl.WebUINodeConfiguration;
 import org.knime.core.webui.node.impl.WebUINodeModel;
+import org.knime.filehandling.core.connections.FSConnection;
+import org.knime.filehandling.core.connections.FSFileSystem;
 import org.knime.filehandling.core.connections.FSFiles;
+import org.knime.filehandling.core.defaultnodesettings.FileSystemHelper;
 import org.pm4knime.portobject.XLogPortObject;
 import org.pm4knime.util.NodeSettingsUtils.ExistingOutputFileHandlingMode;
-import org.pm4knime.util.WriterUtil;
-import org.deckfour.xes.out.XMxmlGZIPSerializer;
-import org.deckfour.xes.out.XMxmlSerializer;
 
- // uses the restricted WebUI API
+@SuppressWarnings("restriction")
 final class MXMLWriterNodeModel extends WebUINodeModel<MXMLWriterNodeSettings> {
 
-	protected final String[] FILE_SUFFIXES = new String[] { ".mxml", ".mxml.gz" };
+	private static final NodeLogger LOGGER = NodeLogger.getLogger(MXMLWriterNodeModel.class);
 
 	MXMLWriterNodeModel(final WebUINodeConfiguration config) {
 		super(config, MXMLWriterNodeSettings.class);
@@ -36,13 +39,22 @@ final class MXMLWriterNodeModel extends WebUINodeModel<MXMLWriterNodeSettings> {
 	@Override
 	protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs, final MXMLWriterNodeSettings settings)
 			throws InvalidSettingsException {
-		CheckUtils.checkSettingNotNull(settings.m_outputFile, "Output path must be present.");
-		CheckUtils.checkSetting(StringUtils.isNotBlank(settings.m_outputFile), "Output path may not be blank: \"%s\"",
-				settings.m_outputFile);
+		
+		if (settings.m_outputFile == null || settings.m_outputFile.getFSLocation() == null) {
+			throw new InvalidSettingsException("Please specify a path to the output file!");
+		}
+		
+		String outputPath = settings.m_outputFile.getFSLocation().getPath();
+		if (StringUtils.isEmpty(outputPath)) {
+			throw new InvalidSettingsException("Output path may not be blank!");
+		}
 
-		final var outputPath = pathWithExtension(settings.m_outputFile, settings.getExtension(), settings);
-		final var url = WriterUtil.toURL(outputPath, WriterUtil::handleInvalidPathSetting);
-		WriterUtil.toURI(url, WriterUtil::checkLocalOrKNIMEURL, WriterUtil::handleInvalidURLSetting);
+		String expectedExt = settings.getExtension();
+		if (!outputPath.endsWith(expectedExt)) {
+			setWarningMessage(
+				String.format("Output file path did not have the correct file extension \"%s\", it will be appended.", expectedExt)
+			);
+		}
 
 		return new PortObjectSpec[0];
 	}
@@ -50,37 +62,46 @@ final class MXMLWriterNodeModel extends WebUINodeModel<MXMLWriterNodeSettings> {
 	@Override
 	protected PortObject[] execute(final PortObject[] inData, final ExecutionContext exec,
 			final MXMLWriterNodeSettings settings) throws Exception {
-		CheckUtils.checkArgumentNotNull(settings.m_outputFile, "Output path must be present.");
-		CheckUtils.checkArgument(StringUtils.isNotBlank(settings.m_outputFile), "Output path may not be blank: \"%s\"",
-				settings.m_outputFile);
+		
+		CheckUtils.checkArgumentNotNull(settings.m_outputFile, "Output file selection must be present.");
+		
+		var fsLocation = settings.m_outputFile.getFSLocation();
+		CheckUtils.checkArgumentNotNull(fsLocation, "File system location must be present.");
+		
+		String outputPath = fsLocation.getPath();
+		CheckUtils.checkArgument(StringUtils.isNotBlank(outputPath), "Output path may not be blank!");
 
-		final var outputPath = pathWithExtension(settings.m_outputFile, settings.getExtension(), settings);
-		CheckUtils.checkDestinationFile(outputPath,
-				settings.m_existingFileHandlingMode == ExistingOutputFileHandlingMode.OVERWRITE);
-
-		final var url = WriterUtil.toURL(outputPath,
-				(ex, invalid) -> WriterUtil.handleInvalidPath(ex, invalid, () -> createMessageBuilder()));
-		final var outputURI = WriterUtil.toURI(url, WriterUtil::checkLocalOrKNIMEURL,
-				(e, invalidUrl) -> WriterUtil.handleInvalidURL(e, invalidUrl, () -> createMessageBuilder()));
+		String pathWithExt = pathWithExtension(outputPath, settings);
+		
+		FSConnection connection = FileSystemHelper.retrieveFSConnection(Optional.empty(), fsLocation)
+				.orElseThrow(() -> new IOException("File system is not available"));
+		FSFileSystem<?> fileSystem = connection.getFileSystem();
+		
+		final Path filePath = fileSystem.getPath(pathWithExt);
+		
+		if (FSFiles.exists(filePath) && 
+			settings.m_existingFileHandlingMode != ExistingOutputFileHandlingMode.OVERWRITE) {
+			throw new InvalidSettingsException(
+				"Output file '" + filePath + "' exists and must not be overwritten due to user settings."
+			);
+		}
 
 		final var tempDir = FileUtil.createTempDir("MXMLWriterNode-").toPath();
 		XLogPortObject logData = (XLogPortObject) inData[0];
-		
-			
+
 		try {
 			if (logData.getLog() != null) {
-				Path path = Paths.get(outputURI);
 				OutputStream outStream;
 				try {
-					outStream = FSFiles.newOutputStream(path);
+					outStream = FSFiles.newOutputStream(filePath);
 					writeToFile(outStream, logData.getLog(), settings);
+					outStream.close();
+					LOGGER.info("Successfully wrote MXML file to: " + filePath);
 				} catch (final FileAlreadyExistsException e) {
 					throw new InvalidSettingsException("Output file '" + e.getFile()
 							+ "' exists and must not be overwritten due to user settings.", e);
 				}
-				outStream.close();
 			}
-
 		} finally {
 			exec.setProgress(1, (String) null);
 			FileUtil.deleteRecursively(tempDir.toFile());
@@ -91,33 +112,21 @@ final class MXMLWriterNodeModel extends WebUINodeModel<MXMLWriterNodeSettings> {
 
 	protected void writeToFile(OutputStream outputStream, XLog log, final MXMLWriterNodeSettings settings)
 			throws IOException {
-		
-		System.out.println("write to file running");
-		
 		if (settings.m_compressWithGzipChecker) {
 			XSerializer logSerializer = new XMxmlGZIPSerializer();
 			logSerializer.serialize(log, outputStream);
-			outputStream.close();
 		} else {
 			XSerializer logSerializer = new XMxmlSerializer();
 			logSerializer.serialize(log, outputStream);
-			outputStream.close();
 		}
 	}
 
-	private String pathWithExtension(final String path, String ext, final MXMLWriterNodeSettings settings) {
-
-		if (settings.m_compressWithGzipChecker)
-			ext = ".mxml.gz";
-		else
-			ext = ".mxml";
-
-		if (!path.toLowerCase(Locale.US).endsWith(ext)) {
-			setWarningMessage(
-					String.format("Output file path did not have the correct file extension, appending \"%s\".", ext));
+	private String pathWithExtension(final String path, final MXMLWriterNodeSettings settings) {
+		String ext = settings.getExtension();
+		
+		if (!path.endsWith(ext)) {
 			return path + ext;
 		}
 		return path;
 	}
-
 }
