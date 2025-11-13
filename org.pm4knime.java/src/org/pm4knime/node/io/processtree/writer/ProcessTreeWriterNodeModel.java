b86082cbd88aa21f79c9b08f92b129ef0c2804e9
samadrita.saha <samadrita.saha@rwth-aachen.de>
2025-11-13 08:01:32 +0100

    updated process tree writer with FileSelection

--- org.pm4knime.java/src/org/pm4knime/node/io/processtree/writer/ProcessTreeWriterNodeModel.java
+++ org.pm4knime.java/src/org/pm4knime/node/io/processtree/writer/ProcessTreeWriterNodeModel.java
@@ -1,26 +1,32 @@
 package org.pm4knime.node.io.processtree.writer;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Locale;
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
 import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.util.CheckUtils;
 import org.knime.core.util.FileUtil;
 import org.knime.core.webui.node.impl.WebUINodeConfiguration;
 import org.knime.core.webui.node.impl.WebUINodeModel;
+import org.knime.filehandling.core.connections.FSConnection;
+import org.knime.filehandling.core.connections.FSFileSystem;
 import org.knime.filehandling.core.connections.FSFiles;
+import org.knime.filehandling.core.defaultnodesettings.FileSystemHelper;
 import org.pm4knime.portobject.ProcessTreePortObject;
 import org.pm4knime.util.NodeSettingsUtils.ExistingOutputFileHandlingMode;
-import org.pm4knime.util.WriterUtil;
 
- // uses the restricted WebUI API
+@SuppressWarnings("restriction")
 final class ProcessTreeWriterNodeModel extends WebUINodeModel<ProcessTreeWriterNodeSettings> {
+
+	private static final NodeLogger LOGGER = NodeLogger.getLogger(ProcessTreeWriterNodeModel.class);
 
 	ProcessTreeWriterNodeModel(final WebUINodeConfiguration config) {
 		super(config, ProcessTreeWriterNodeSettings.class);
@@ -29,66 +35,83 @@ final class ProcessTreeWriterNodeModel extends WebUINodeModel<ProcessTreeWriterN
 	@Override
 	protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs, final ProcessTreeWriterNodeSettings settings)
 			throws InvalidSettingsException {
-		CheckUtils.checkSettingNotNull(settings.m_outputFile, "Output path must be present.");
-		CheckUtils.checkSetting(StringUtils.isNotBlank(settings.m_outputFile), "Output path may not be blank: \"%s\"",
-				settings.m_outputFile);
+		
+		if (settings.m_outputFile == null || settings.m_outputFile.getFSLocation() == null) {
+			throw new InvalidSettingsException("Please specify a path to the output file!");
+		}
+		
+		String outputPath = settings.m_outputFile.getFSLocation().getPath();
+		if (StringUtils.isEmpty(outputPath)) {
+			throw new InvalidSettingsException("Output path may not be blank!");
+		}
 
-		final var outputPath = pathWithExtension(settings.m_outputFile, settings.getExtension());
-		final var url = WriterUtil.toURL(outputPath, WriterUtil::handleInvalidPathSetting);
-		WriterUtil.toURI(url, WriterUtil::checkLocalOrKNIMEURL, WriterUtil::handleInvalidURLSetting);
+		String expectedExt = settings.getExtension();
+		if (!outputPath.endsWith(expectedExt)) {
+			setWarningMessage(
+				String.format("Output file path did not have the correct file extension \"%s\", it will be appended.", expectedExt)
+			);
+		}
 
 		return new PortObjectSpec[0];
 	}
 
 	@Override
-    protected PortObject[] execute(final PortObject[] inData, final ExecutionContext exec,
-        final ProcessTreeWriterNodeSettings settings) throws Exception {
-        CheckUtils.checkArgumentNotNull(settings.m_outputFile, "Output path must be present.");
-        CheckUtils.checkArgument(StringUtils.isNotBlank(settings.m_outputFile), "Output path may not be blank: \"%s\"",
-            settings.m_outputFile);
+	protected PortObject[] execute(final PortObject[] inData, final ExecutionContext exec,
+			final ProcessTreeWriterNodeSettings settings) throws Exception {
+		
+		CheckUtils.checkArgumentNotNull(settings.m_outputFile, "Output file selection must be present.");
+		
+		var fsLocation = settings.m_outputFile.getFSLocation();
+		CheckUtils.checkArgumentNotNull(fsLocation, "File system location must be present.");
+		
+		String outputPath = fsLocation.getPath();
+		CheckUtils.checkArgument(StringUtils.isNotBlank(outputPath), "Output path may not be blank!");
 
+		String pathWithExt = pathWithExtension(outputPath, settings);
+		
+		FSConnection connection = FileSystemHelper.retrieveFSConnection(Optional.empty(), fsLocation)
+				.orElseThrow(() -> new IOException("File system is not available"));
+		FSFileSystem<?> fileSystem = connection.getFileSystem();
+		
+		final Path filePath = fileSystem.getPath(pathWithExt);
+		
+		if (FSFiles.exists(filePath) && 
+			settings.m_existingFileHandlingMode != ExistingOutputFileHandlingMode.OVERWRITE) {
+			throw new InvalidSettingsException(
+				"Output file '" + filePath + "' exists and must not be overwritten due to user settings."
+			);
+		}
 
-        final var outputPath = pathWithExtension(settings.m_outputFile, settings.getExtension());
-        CheckUtils.checkDestinationFile(outputPath,
-            settings.m_existingFileHandlingMode == ExistingOutputFileHandlingMode.OVERWRITE);
+		final var tempDir = FileUtil.createTempDir("ProcessTreeWriterNode-").toPath();
+		ProcessTreePortObject m_ptPort = (ProcessTreePortObject) inData[0];
 
-        final var url = WriterUtil.toURL(outputPath,
-            (ex, invalid) -> WriterUtil.handleInvalidPath(ex, invalid, () -> createMessageBuilder()));
-        final var outputURI = WriterUtil.toURI(url, WriterUtil::checkLocalOrKNIMEURL,
-            (e, invalidUrl) -> WriterUtil.handleInvalidURL(e, invalidUrl, () -> createMessageBuilder()));
+		try {
+			if (m_ptPort.getTree() != null) {
+				OutputStream outStream;
+				try {
+					outStream = FSFiles.newOutputStream(filePath);
+					m_ptPort.save_from_stream(outStream);
+					outStream.close();
+					LOGGER.info("Successfully wrote Process Tree file to: " + filePath);
+				} catch (final FileAlreadyExistsException e) {
+					throw new InvalidSettingsException("Output file '" + e.getFile()
+							+ "' exists and must not be overwritten due to user settings.", e);
+				}
+			}
+		} finally {
+			exec.setProgress(1, (String) null);
+			FileUtil.deleteRecursively(tempDir.toFile());
+		}
 
-        final var tempDir = FileUtil.createTempDir("ProcessTreeWriterNode-").toPath();
-        
-        ProcessTreePortObject m_ptPort = (ProcessTreePortObject) inData[0];
-        
-        try {
-        	if(m_ptPort.getTree() != null) {
-        		Path path = Paths.get(outputURI); 
-            	OutputStream outStream;            
-            	try {
-                    outStream = FSFiles.newOutputStream(path);
-                } catch (final FileAlreadyExistsException e) {
-                    throw new InvalidSettingsException(
-                        "Output file '" + e.getFile() + "' exists and must not be overwritten due to user settings.", e);
-                }
-            	m_ptPort.save_from_stream(outStream);
-            	outStream.close();
-            }
-        } finally {
-            exec.setProgress(1, (String)null);
-            FileUtil.deleteRecursively(tempDir.toFile());
-        }
-        
-        return new PortObject[0];
-    }
+		return new PortObject[0];
+	}
 
-	private String pathWithExtension(final String path, String ext) {
-		if (!path.toLowerCase(Locale.US).endsWith(ext)) {
-			setWarningMessage(
-					String.format("Output file path did not have the correct file extension, appending \"%s\".", ext));
+	private String pathWithExtension(final String path, final ProcessTreeWriterNodeSettings settings) {
+		String ext = settings.getExtension();
+		
+		if (!path.endsWith(ext)) {
 			return path + ext;
 		}
 		return path;
 	}
-
 }
