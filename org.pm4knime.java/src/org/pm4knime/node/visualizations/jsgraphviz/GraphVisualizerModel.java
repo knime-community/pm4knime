c08e99bfe680058399df28511994ddaf77274e07
humam.kourani <humam.kourani@fit.fraunhofer.de>
2026-03-25 23:58:15 +0100

    deleted unused files

--- org.pm4knime.java/src/org/pm4knime/node/visualizations/jsgraphviz/GraphVisualizerModel.java
+++ org.pm4knime.java/src/org/pm4knime/node/visualizations/jsgraphviz/GraphVisualizerModel.java
@@ -1,8 +1,12 @@
 package org.pm4knime.node.visualizations.jsgraphviz;
 
 import org.knime.base.data.xml.SvgCell;
+import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortType;
 import org.knime.core.node.port.PortTypeRegistry;
+import org.knime.core.node.port.image.ImagePortObject;
 import org.knime.core.node.port.image.ImagePortObjectSpec;
 import org.knime.node.DefaultModel;
 import org.pm4knime.portobject.AbstractJSONPortObject;
@@ -22,11 +26,17 @@ public final class GraphVisualizerModel {
         try {
             final var portObject = (AbstractJSONPortObject)i.getInPortObject(0);
             final var inputType = PortTypeRegistry.getInstance().getPortType(portObject.getClass());
-            final var imagePort = JSGraphVizImageNodeModel.render(portObject, inputType, i.getExecutionContext());
+            final var imagePort = render(portObject, inputType, i.getExecutionContext());
             o.setOutData(0, imagePort);
             o.setInternalData(portObject);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
-        }
+        }       
+    }
+    
+    static ImagePortObject render(final AbstractJSONPortObject portObject, final PortType inputType,
+        final ExecutionContext exec) throws Exception {
+
+        return null;
     }
 }
