3204da153d6662e734bb16680a2a80ee3976e6ac
humam.kourani <humam.kourani@fit.fraunhofer.de>
2026-03-26 17:24:42 +0100

    disable view for alpha miner

--- org.pm4knime.java/src/org/pm4knime/node/discovery/alpha/table/AlphaMinerTableNodeFactory.java
+++ org.pm4knime.java/src/org/pm4knime/node/discovery/alpha/table/AlphaMinerTableNodeFactory.java
@@ -3,7 +3,6 @@ package org.pm4knime.node.discovery.alpha.table;
 import org.knime.node.DefaultNode;
 import org.knime.node.DefaultNodeFactory;
 import org.pm4knime.node.discovery.defaultminer.DefaultTableMinerNodeModel;
-import org.pm4knime.node.visualizations.common.ModernViews;
 import org.pm4knime.portobject.PetriNetPortObject;
 
 public class AlphaMinerTableNodeFactory extends DefaultNodeFactory {
@@ -26,7 +25,7 @@ public class AlphaMinerTableNodeFactory extends DefaultNodeFactory {
                         new AlphaMinerTableNodeModel(AlphaMinerTableNodeSettings.class)))
                     .execute((i, o) -> DefaultTableMinerNodeModel.execute(i, o,
                         new AlphaMinerTableNodeModel(AlphaMinerTableNodeSettings.class))))
-                .addView(v -> ModernViews.graph(v, AlphaMinerTableNodeFactory.class, "Petri net view"))
+//                .addView(v -> ModernViews.graph(v, AlphaMinerTableNodeFactory.class, "Petri net view"))
                 .nodeType(NodeType.Learner));
     }
 }
