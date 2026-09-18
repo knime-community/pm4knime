ed9fd81c5d38efbd3034861dd32568c8014e23cb
Stefan Helfrich <stefan.helfrich@knime.com>
2026-09-18 10:23:06 +0200

    Add stub for PetriNetConverter

--- org.pm4knime.java/python/utils/petri_net_type.py
+++ org.pm4knime.java/python/utils/petri_net_type.py
@@ -1,5 +1,7 @@
 import knime.api.types as kt
-
+import knime.extension as knext
+import knime.extension.ports as kp
+from typing import List, Dict, Any
 
 class PetriNetPythonCell:
     def __init__(self, string_pn):
@@ -9,4 +11,8 @@ class PetriNetPythonCell:
 class PetriNetPythonFactory(kt.PythonValueFactory):
     def __init__(self):
         kt.PythonValueFactory.__init__(self, PetriNetPythonCell)
-        
+
+
+class PetriNetPortConverter(kp.PortObjectDecoder, kp.PortObjectEncoder):  
+    def __init__(self):
+        pass
